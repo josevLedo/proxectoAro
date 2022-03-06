c4bd2ae80ed0276eee144435842ae99850f5bfdb
Jose Ledo <a21josevl@iessanclemente.net>
2022-03-06 12:48:37 +0100

    "Encapsulados os tres campos da clase ( coordenadaX, coordenadaY, radio). Tests ok"

--- src/main/java/aro/Aro.java
+++ src/main/java/aro/Aro.java
@@ -5,13 +5,55 @@ package aro;
  * @author jose
  */
 public class Aro {
-
-    private static final double LIMITERADIO = 0.0;
-    public static final double MINIMO = LIMITERADIO;
-
+    
     private int coordenadaX;
     private int coordenadaY;
     private double radio;
+
+    /**
+     * @return the coordenadaX
+     */
+    public int getCoordenadaX() {
+        return coordenadaX;
+    }
+
+    /**
+     * @param coordenadaX the coordenadaX to set
+     */
+    public void setCoordenadaX(int coordenadaX) {
+        this.coordenadaX = coordenadaX;
+    }
+
+    /**
+     * @return the coordenadaY
+     */
+    public int getCoordenadaY() {
+        return coordenadaY;
+    }
+
+    /**
+     * @param coordenadaY the coordenadaY to set
+     */
+    public void setCoordenadaY(int coordenadaY) {
+        this.coordenadaY = coordenadaY;
+    }
+
+    /**
+     * @return the radio
+     */
+    public double getRadio() {
+        return radio;
+    }
+
+    /**
+     * @param radio the radio to set
+     */
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    private static final double LIMITERADIO = 0.0;
+    public static final double MINIMO = LIMITERADIO;
 
     public Aro() {
     }
@@ -23,32 +65,32 @@ public class Aro {
     }
 
     public void establecerX(int valorX) {
-        coordenadaX=valorX;
+        setCoordenadaX(valorX);
     }
 
     public int obterX() {
-        return coordenadaX;
+        return getCoordenadaX();
     }
 
     public void establecerY(int valorY) {
-        coordenadaY=valorY;
+        setCoordenadaY(valorY);
     }
 
     public int obterY() {
-        return coordenadaY;
+        return getCoordenadaY();
     }
 
     public void establecerRadio(double valorRadio) {
 
-        radio=(valorRadio < MINIMO ? MINIMO : valorRadio);
+        setRadio(valorRadio < MINIMO ? MINIMO : valorRadio);
     }
 
     public double obterRadio() {
-        return radio;
+        return getRadio();
     }
 
     public double obterDiametro() {
-        return radio * 2;
+        return getRadio() * 2;
     }
 
     public double obterCircunferencia() {
@@ -56,16 +98,16 @@ public class Aro {
     }
 
     public double obterSuperficie() {
-        return Math.PI * radio * radio;
+        return Math.PI * getRadio() * getRadio();
     }
 
     @Override
     public String toString() {
-        return "Centro = [" + coordenadaX + "," + coordenadaY + "]; Radio = " + radio;
+        return "Centro = [" + getCoordenadaX() + "," + getCoordenadaY() + "]; Radio = " + getRadio();
     }
 
     public void trasladarCentro(int trasladarX, int trasladarY){
-        coordenadaX+= trasladarX;
-        coordenadaY+= trasladarY;
+        setCoordenadaX(getCoordenadaX() + trasladarX);
+        setCoordenadaY(getCoordenadaY() + trasladarY);
     }
 }
